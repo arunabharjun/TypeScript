724d61f73947317f067d4b4f521e1419d8a23525
arunabharjun <sunnyarj114@gmail.com>
2020-09-01 09:29:27 +0530

    Added Popup content and implements clause for User & Company class with Mappable interface

--- Maps_App/src/CustomMap.ts
+++ Maps_App/src/CustomMap.ts
@@ -1,10 +1,11 @@
 // Instructions to every other class
 // on how they can be an argument to 'addMarker
-interface Mappable {
+export interface Mappable {
 	location: {
 		lat: number;
 		lng: number;
 	};
+	markerContent(): string;
 }
 
 export class CustomMap {
@@ -33,7 +34,7 @@ export class CustomMap {
 
 		marker.addListener('click', () => {
 			const infoWindow = new google.maps.InfoWindow({
-				content: 'Some info about the marker'
+				content: mappable.markerContent()
 			});
 
 			infoWindow.open(this.googleMap, marker);
