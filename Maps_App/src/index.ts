e9a1918db27177cc6fe87098c981f8196acd46ea
arunabharjun <sunnyarj114@gmail.com>
2020-09-01 08:33:07 +0530

    Using same function to render markers for both user & company

--- Maps_App/src/index.ts
+++ Maps_App/src/index.ts
@@ -8,5 +8,5 @@ console.log(user);
 console.log(company);
 
 const customMap = new CustomMap('map');
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+customMap.addMarker(user);
+customMap.addMarker(company);
